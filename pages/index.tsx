b56aa017ca4756b3c4639419e48584231ae128fc
Iago saraiva <iago.saraiva@pmovil.com.br>
2023-03-30 15:56:47 -0300

    Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import HeaderNoAuth from '@/src/components/homeNoAuth/headerNoAuth';
 import Head from 'next/head'
 import styles from '../styles/HomeNoAuth.module.scss'
 
@@ -5,11 +6,17 @@ const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title> Home</title>
+        <title> DevFlix </title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property='og:title' content='DevFlix' key="title"/>
+        <meta name='description' 
+        content='Tenha acesso aos melhores conteudos de programação de uma forma simples e fácil'/>
       </Head>
-      <main> </main>
+      <main>
+        <HeaderNoAuth/>
+      </main>
     </>
-  )
+  );
 }
 
 export default HomeNoAuth;
