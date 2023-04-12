5a725131fc6ba77988534d9a89af8bf4eec4fa5b
Iago saraiva <iago.saraiva@pmovil.com.br>
2023-04-12 13:33:54 -0300

    Deploy features

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,8 @@ import axios from "axios";
 const baseURL = process.env.NEXT_PUBLIC_BASEURL
 
 const api = axios.create({
-    baseURL
+    baseURL: baseURL,
+    insecureHTTPParser: true,
 });
 
 export default api
